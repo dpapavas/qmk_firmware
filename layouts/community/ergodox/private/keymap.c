3de7498761055f08e984bc4f15d51afe4d7eade2
Dimitris Papavasiliou <d.papavasiliou@trebbble.co>
2018-03-28 13:00:33 +0300

    Updated the private keymap.

--- layouts/community/ergodox/private/keymap.c
+++ layouts/community/ergodox/private/keymap.c
@@ -5,67 +5,74 @@
 #define BASE 0 // default layer
 #define FN1  1 // media layer
 
-#define CAPS_GUI MT(MOD_LGUI, KC_CAPS)
-#define UP_SFT MT(MOD_RSFT, KC_UP)
-#define RGHT_SFT MT(MOD_LSFT, KC_RGHT)
+#define CAPS_SFT MT(MOD_LSFT, KC_CAPS)
+#define QUOT_SFT MT(MOD_RSFT, KC_QUOT)
 #define DOWN_ALT MT(MOD_RALT, KC_DOWN)
+#define EQL_ALT MT(MOD_RALT, KC_EQL)
+#define ESC_FN LT(FN1, KC_ESC)
+#define GRV_FN LT(FN1, KC_GRV)
+#define RBRC_FN LT(FN1, KC_RBRC)
+#define PGDN_GUI MT(MOD_LGUI, KC_PGDN)
+#define DOWN_GUI MT(MOD_RGUI, KC_DOWN)
+#define HOME_GUI MT(MOD_LGUI, KC_HOME)
+#define RGHT_GUI MT(MOD_RGUI, KC_RIGHT)
+#define LBRC_ALT MT(MOD_LALT, KC_LBRC)
 #define LEFT_ALT MT(MOD_LALT, KC_LEFT)
 #define LPRN_CTL MT(MOD_LCTL, LSFT(KC_9))
-#define RPRN_CTL MT(MOD_RCTL, RSFT(KC_0))
-#define LBRC_ALT MT(MOD_LALT, KC_LBRC)
-#define RBRC_ALT MT(MOD_RALT, KC_RBRC)
 #define MINS_ALT MT(MOD_LALT, KC_MINS)
-#define EQL_ALT MT(MOD_RALT, KC_EQL)
-#define LBRC_GUI MT(MOD_RGUI, KC_LBRC)
-#define RBRC_GUI MT(MOD_LGUI, KC_RBRC)
+#define RBRC_ALT MT(MOD_RALT, KC_RBRC)
+#define RGHT_SFT MT(MOD_LSFT, KC_RGHT)
+#define RPRN_CTL MT(MOD_RCTL, RSFT(KC_0))
+#define UP_SFT MT(MOD_RSFT, KC_UP)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  | Home |           |  End |   6  |   7  |   8  |   9  |   0  |  Esc   |
+ * |   Fn   |   1  |   2  |   3  |   4  |   5  | `/FN |           |Esc/FN|   6  |   7  |   8  |   9  |   0  |   Fn   |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | LALT |           | RALT |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  | LALT |           | RALT |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
- * |Caps/Gui|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |Caps/Sft|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | '/Shift|
  * |--------+------+------+------+------+------| LCTL |           | RCTL |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |  (   |           |  )   |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LCtrl |  Fn  | SysRq|[/LGui| -/Alt|                                       | =/Alt|]/RGui| Pause|  Fn  | RCtrl|
+ *   |LCtrl | SysRq|  Ins |   [  | -/Alt|                                       | =/Alt| ]/Fn | Del  | Pause| RCtrl|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Ins  | Del  |       | Left | Right|
+ *                                        |Home/G|  End |       | Left |Rght/G|
  *                                 ,------|------+------|       |------+------+------.
  *                                 |      |      | PgUp |       |  Up  |      |      |
- *                                 |Enter | Del  |------|       |------|BSpace| Space|
- *                                 |      |      | PgDn |       | Down |      |      |
+ *                                 |BSpace|  Del |------|       |------| Enter| Space|
+ *                                 |      |      |PD/Gui|       |Dn/Gui|      |      |
  *                                 `--------------------'       `--------------------'
  */
 
     [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // Left hand
-        KC_GRV,   KC_1,    KC_2,      KC_3,     KC_4,     KC_5,    KC_HOME,
-        KC_TAB,   KC_Q,    KC_W,      KC_E,     KC_R,     KC_T,    LBRC_ALT,
-        CAPS_GUI, KC_A,    KC_S,      KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,    KC_X,      KC_C,     KC_V,     KC_B,    LPRN_CTL,
-        KC_LCTL,  TT(FN1), KC_SYSREQ, LBRC_GUI, MINS_ALT,
-        KC_INS,   KC_DEL,
+        TT(FN1),  KC_1,      KC_2,      KC_3,     KC_4,     KC_5,    GRV_FN,
+        KC_TAB,   KC_Q,      KC_W,      KC_E,     KC_R,     KC_T,    LBRC_ALT,
+        CAPS_SFT, KC_A,      KC_S,      KC_D,     KC_F,     KC_G,
+        KC_LSFT,  KC_Z,      KC_X,      KC_C,     KC_V,     KC_B,    LPRN_CTL,
+        KC_LCTL,  KC_SYSREQ, KC_INS,    KC_LBRC,  MINS_ALT,
+        HOME_GUI, KC_END,
         KC_PGUP,
-        KC_ENT,   KC_DEL,  KC_PGDN,
+        KC_BSPC,  KC_DEL,    PGDN_GUI,
 
         // Right hand
-        KC_END,   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_ESC,
+        ESC_FN,   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    TT(FN1),
         RBRC_ALT, KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,    KC_BSLS,
-                  KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN, KC_QUOT,
+                  KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN, QUOT_SFT,
         RPRN_CTL, KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,
-        EQL_ALT,  RBRC_GUI, KC_PAUSE, TT(FN1),  KC_RCTL,
-        KC_LEFT,  KC_RIGHT,
+        EQL_ALT,  RBRC_FN,  KC_DEL,   KC_PAUSE, KC_RCTL,
+        KC_LEFT,  RGHT_GUI,
         KC_UP,
-        KC_DOWN,  KC_BSPC, KC_SPC),
+        DOWN_GUI, KC_ENT, KC_SPC),
+
 /* Keymap 1: Fn Keys, media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  Esc   |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |        |      |      | MsUp |      |      |      |           |      |      |      |  Up  |      |      |   F11  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -83,10 +90,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       | VolDn|      |      |
  *                                 `--------------------'       `--------------------'
  */
+
 // FN1 Layer
     [FN1] = LAYOUT_ergodox(
         // Left hand
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN3, KC_BTN2, KC_TRNS, KC_TRNS,
@@ -96,9 +104,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS,
         // Right hand
         KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_F11,
-                 KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_F12,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_F12,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_MPRV, KC_MNXT,
         KC_VOLU,
@@ -118,8 +126,6 @@ void matrix_scan_user(void) {
 
     uint8_t layer = biton32(layer_state);
 
-    ergodox_board_led_off();
-    ergodox_right_led_1_off();
     switch (layer) {
         // TODO: Make this relevant to the ErgoDox EZ.
     case 1:
@@ -127,6 +133,7 @@ void matrix_scan_user(void) {
         break;
     default:
         // none
+        ergodox_right_led_1_off();
         break;
     }
 
--- layouts/community/ergodox/private/rules.mk
+++ layouts/community/ergodox/private/rules.mk
@@ -1,7 +1,6 @@
 # Having a file like this allows you to override Makefile definitions
 # for your own particular keymap
 
-SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 #COMMAND_ENABLE   = no   # Commands for debug and configuration
 UNICODE_ENABLE   = no
 #CONSOLE_ENABLE   = yes
